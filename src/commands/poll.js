76c2ae04cc29136e1bbc1fbe1b845d2e4d55df85
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-01-26 23:06:59 -0800

    added mroe emojis and playing with the idea of success message

--- src/commands/poll.js
+++ src/commands/poll.js
@@ -23,7 +23,7 @@ module.exports = {
                     obj.map((item, i) => {
                         bracketOptions.push(item)
                     })
-                    let emojiList = ["😍", "🥺", "😁", "😂"]
+                    let emojiList = ["😍", "🥺", "😁", "😂", "👍", "😎", "👽", "😏", "📝", "😘", "😈", "🤓", "🤯", "❤️", "👌", "😩", "🤙", "🤷‍♀️", "😛", "🥳", "🤔", "🥁", "😭", "😺", "😤", "💃", "💅", "😆", "💪", "🙌", "👀", "👻"]
                     let fitEmojiList = emojiList.slice(0, bracketOptions.length)
             
                     let result = [];
@@ -37,11 +37,11 @@ module.exports = {
                     message.channel.send(embeded)
                     .then(async msg => {
                         const filter = (reaction) => {
-                            return emojiList.includes(reaction.emoji.name)
+                            return fitEmojiList.includes(reaction.emoji.name)
                         }
                         setTimeout(function() {message.channel.send(`10 seconds left until poll "${bracketTitle}" closes!`)}, 50000)
                         const collector = msg.createReactionCollector(filter, {time: 60000})
-                        let emoji1 = emoji2 = emoji3 = emoji4 = 0
+                        let emoji1 = emoji2 = emoji3 = emoji4 = emoji5 = emoji6 = emoji7 = emoji8 = emoji9 = emoji10 = emoji11 = emoji12 = emoji13 = emoji14 = emoji15 = emoji16 = emoji17 = emoji18 = emoji19 = emoji20 = emoji21 = emoji22 = emoji23 = emoij24 = emoji25 = emoji26 = emoji27 = emoji28 = emoji29 = emoji30 = emoji31 = emoji32 = 0
                        
                         collector.on('collect', (reaction, user) => {
                             if (reaction.emoji.name === '😍') {
@@ -52,10 +52,67 @@ module.exports = {
                                 emoji3 += 1
                             } else if (reaction.emoji.name === '😂') {
                                 emoji4 += 1
+                            } else if (reaction.emoji.name === '👍') {
+                                emoji5 += 1
+                            } else if (reaction.emoji.name === '😎') {
+                                emoji6 += 1
+                            } else if (reaction.emoji.name === '👽') {
+                                emoji7 += 1
+                            } else if (reaction.emoji.name === '😏') {
+                                emoji8 += 1
+                            } else if (reaction.emoji.name === '📝') {
+                                emoji9 += 1
+                            }else if (reaction.emoji.name === '😘') {
+                                emoji10 += 1
+                            }else if (reaction.emoji.name === '😈') {
+                                emoji11 += 1
+                            }else if (reaction.emoji.name === '🤓') {
+                                emoji12 += 1
+                            }else if (reaction.emoji.name === '🤯') {
+                                emoji13 += 1
+                            }else if (reaction.emoji.name === '❤️') {
+                                emoji14 += 1
+                            }else if (reaction.emoji.name === '👌') {
+                                emoji15 += 1
+                            }else if (reaction.emoji.name === '😩') {
+                                emoji16 += 1
+                            }else if (reaction.emoji.name === '🤙') {
+                                emoji17 += 1
+                            }else if (reaction.emoji.name === '🤷‍♀️') {
+                                emoji18 += 1
+                            }else if (reaction.emoji.name === '😛') {
+                                emoji19 += 1
+                            }else if (reaction.emoji.name === '🥳') {
+                                emoji20 += 1
+                            }else if (reaction.emoji.name === '🤔') {
+                                emoji21 += 1
+                            }else if (reaction.emoji.name === '🥁') {
+                                emoji22 += 1
+                            }else if (reaction.emoji.name === '😭') {
+                                emoji23 += 1
+                            }else if (reaction.emoji.name === '😺') {
+                                emoji24 += 1
+                            }else if (reaction.emoji.name === '😤') {
+                                emoji25 += 1
+                            }else if (reaction.emoji.name === '💃') {
+                                emoji26 += 1
+                            }else if (reaction.emoji.name === '💅') {
+                                emoji27 += 1
+                            }else if (reaction.emoji.name === '😆') {
+                                emoji28 += 1
+                            }else if (reaction.emoji.name === '💪') {
+                                emoji29 += 1
+                            }else if (reaction.emoji.name === '🙌') {
+                                emoji30 += 1
+                            }else if (reaction.emoji.name === '👀') {
+                                emoji31 += 1
+                            }else if (reaction.emoji.name === '👻') {
+                                emoji32 += 1
                             }
                         })
                         collector.on('end', collected => {
                             let emojis = [];
+                            let itEmoji = 
                             emojis.push(emoji1, emoji2, emoji3, emoji4)
                             bracketOptions.forEach(function(key, i){
                                 let votingChoice = {
@@ -64,7 +121,7 @@ module.exports = {
                                 }
                                 axios.put(`http://localhost:5000/bracket/${args[0]}/bulkvote`, votingChoice)
                                 .then((res) => {
-                                    console.log(res)
+                                    message.channel.send(`Votes counted for ${votingChoice.option}`)
                                 })
                                 .catch(err => {
                                     console.log(err)
